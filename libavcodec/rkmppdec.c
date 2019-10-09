db7ac3cecb6b2f83edf9456d49e00ce46bbb0334
Jeffy Chen <jeffy.chen@rock-chips.com>
2019-10-09 14:20:26 +0800

    avcodec/rkmppdec: Force aligning coded width and height to 64
    The chromium would try to align planes' width and height to 32, which
    might break the plane buffers' contiguous required by RGA.
    
    Passing a 64-aligned coded width and height to avoid that.
    
    Signed-off-by: Jeffy Chen <jeffy.chen@rock-chips.com>

--- libavcodec/rkmppdec.c
+++ libavcodec/rkmppdec.c
@@ -436,6 +436,11 @@ static int rkmpp_retrieve_frame(AVCodecContext *avctx, AVFrame *frame)
             avctx->width = mpp_frame_get_width(mppframe);
             avctx->height = mpp_frame_get_height(mppframe);
 
+            // chromium would align planes' width and height to 32, adding this
+            // hack to avoid breaking the plane buffers' contiguous.
+            avctx->coded_width = FFALIGN(avctx->width, 64);
+            avctx->coded_height = FFALIGN(avctx->height, 64);
+
             decoder->mpi->control(decoder->ctx, MPP_DEC_SET_INFO_CHANGE_READY, NULL);
 
             av_buffer_unref(&decoder->frames_ref);
